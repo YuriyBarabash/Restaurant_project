879e78fdf4ad5d8c92c1be2ceb61599aa87c2244
Vostro <barabashyuriy@gmail.com>
2024-05-02 17:43:54 +0200

    created views blog , blog_singe_page, with pagination , releted posts and extended templates

--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,59 @@
+import random
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Blog
 
 
 def blog(request):
-    blog = Blog.objects.filter(is_published=True)
+    """
+        View function to display a paginated list of published blogs.
+
+        Args:
+            request (HttpRequest): The HTTP request object.
+
+        Returns:
+            HttpResponse: The rendered response containing the paginated blog list.
+
+        Raises:
+            None
+        """
+    blog_list = Blog.objects.filter(is_published=True)
+    paginator = Paginator(blog_list, 6)
+    page = request.GET.get('page')
+    try:
+        blogs = paginator.page(page)
+    except PageNotAnInteger:
+        blogs = paginator.page(1)
+    except EmptyPage:
+        blogs = paginator.page(paginator.num_pages)
     context = {
-        'blog': blog
+        'blogs': blogs
     }
     return render(request, 'blog_index.html', context)
 
 
 def blog_single_page(request, blog_id):
+    """
+        View function to display a single blog post along with related posts.
+
+        Args:
+            request (HttpRequest): The HTTP request object.
+            blog_id (int): The ID of the blog post to be displayed.
+
+        Returns:
+            HttpResponse: The rendered response containing the single blog post and related posts.
+
+        Raises:
+            Http404: If the requested blog post does not exist.
+        """
     blog = get_object_or_404(Blog, id=blog_id)
+    related_blogs = Blog.objects.filter(mark=blog.mark).exclude(id=blog_id)
+    related_blogs_list = list(related_blogs)
+    random_related_blogs = random.sample(related_blogs_list, min(len(related_blogs_list), 3))
     context = {
         'blog': blog,
         'blog_id': blog_id,
+        'related_blogs': related_blogs,
+        'random_related_blogs': random_related_blogs
     }
     return render(request, 'blog_single_page_index.html', context)
